7ffdf5ae8bbc423f2145073d6f3ac7030a8c0e97
Nityam Tripathi <tripathinityam89@gmail.com>
2024-01-30 18:22:38 +0530

    changes to bft

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <>
-    <iframe
+      {/* <iframe
     title='This is unique'
     src="https://embed.ipfscdn.io/ipfs/bafybeicd3qfzelz4su7ng6n523virdsgobrc5pcbarhwqv3dj3drh645pi/?contract=0x71deAF447f10890FF5C06691EA0e60e2C48a8C34&chain=%7B%22name%22%3A%22Mumbai%22%2C%22chain%22%3A%22Polygon%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Fmumbai.rpc.thirdweb.com%2F%24%7BTHIRDWEB_API_KEY%7D%22%5D%2C%22nativeCurrency%22%3A%7B%22name%22%3A%22MATIC%22%2C%22symbol%22%3A%22MATIC%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22maticmum%22%2C%22chainId%22%3A80001%2C%22testnet%22%3Atrue%2C%22slug%22%3A%22mumbai%22%2C%22icon%22%3A%7B%22url%22%3A%22ipfs%3A%2F%2FQmcxZHpyJa8T4i63xqjPYrZ6tKrt55tZJpbXcjSDKuKaf9%2Fpolygon%2F512.png%22%2C%22width%22%3A512%2C%22height%22%3A512%2C%22format%22%3A%22png%22%7D%7D&clientId=7cec8df572e035bea30e6a31289ca908&theme=light&primaryColor=purple"
     width="100%"
     height="600"
     style={{maxwidth:'100%'}}
     frameborder="0"
-></iframe>
+></iframe> */}
+      <iframe
+      title="This is BFT gasless drop"
+        src="https://embed.ipfscdn.io/ipfs/bafybeicd3qfzelz4su7ng6n523virdsgobrc5pcbarhwqv3dj3drh645pi/?contract=0xAb763c5E37A5276bcC02C2029a98a9BFEc26c456&chain=%7B%22name%22%3A%22Bitfinity+Network+Testnet%22%2C%22chain%22%3A%22BFT%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Fbitfinity-network-testnet.rpc.thirdweb.com%2F%24%7BTHIRDWEB_API_KEY%7D%22%5D%2C%22nativeCurrency%22%3A%7B%22name%22%3A%22BITFINITY%22%2C%22symbol%22%3A%22BFT%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22Bitfinity%22%2C%22chainId%22%3A355113%2C%22testnet%22%3Atrue%2C%22slug%22%3A%22bitfinity-network-testnet%22%7D&clientId=7cec8df572e035bea30e6a31289ca908&theme=light&primaryColor=purple"
+        width="100%"
+        height="600px"
+        style={{maxwidth:'100%'}}
+        frameborder="0"
+      ></iframe>
     </>
   );
 }
